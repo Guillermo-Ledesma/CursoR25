c68fb622a05f8e44a7cae3e815faef5a9fb45d6e
quelvc <96quelvili@gmail.com>
2025-01-14 16:26:50 +0100

    modificacion plot #1

--- code/analysis_iris.R
+++ code/analysis_iris.R
@@ -9,3 +9,7 @@ library(tidyverse)
 
 ggplot(dt, aes(x=Petal.Length, fill=Species))+
   geom_density()
+
+ggplot(dt, aes(x=Petal.Length, fill=Species))+
+  geom_density() +
+  scale_fill_manual(values = c("white", "green", "yellow"))
